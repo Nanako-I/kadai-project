d07b4e10a86ef34f579e8759a9f1537042ac55bc
Nanako-I <milklmi.1103775@gmail.com>
2023-02-18 01:00:38 +0900

    html追加

--- src/pages/column.jsx
+++ src/pages/column.jsx
@@ -24,9 +24,9 @@ export const ArticleC = () => {
   );
 };
 
-const article_a="A"
-const article_b="B"
-const article_c="C"
+const article_a="ダウン症は、染色体数の異常によって起こります。ヒトの染色体は、1番目から22番目の染色体まではペアになっていますが、ダウン症候群は21番目の染色体が1本多く3本あり、これが原因の疾患です。"
+const article_b="ダウン症には循環器、消化器、耳鼻咽喉科、整形外科、血液内科などに合併症が生じる可能性があります。"
+const article_c="ダウン症候群は平均寿命も短いとされていましたが、医学の進歩とともに治療や健康管理が充実し、平均寿命も延びてきています。"
 
 export {article_a};
 export {article_b};
